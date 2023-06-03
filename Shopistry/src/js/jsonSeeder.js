2d8f5a934b7a6b56922c70fb88bed7a8b91b5baa
ArnelaOmbasa <116111226+ArnelaOmbasa@users.noreply.github.com>
2023-06-03 20:36:52 +0200

    Add files via upload

---
+++ Shopistry/src/js/jsonSeeder.js
@@ -0,0 +1,22 @@
+jsonSeeder = {
+    "products":[
+        {"id":15,"title":"Canon  EOS 3000d Camera","img":"src/img/camera-1.webp","price":1500,"description":"For its price, the Canon EOS 3000D is a feature packed DSLR that offers almost all the features that Canon’s EOS series is known for. The camera has an 18MP APS-C size CMOS sensor and DIGIC 4+ image processor.","stockQty":48,"category":["Electronics"]},
+        {"id":17,"title":"Laptop","img":"src/img/laptop-1.webp","price":1150,"description":"Lenovo ideapad330C I7 Notebook released in the month of March 2019. The laptop has a screen size of 13.3 inch which gives a resolution of 1920 x 1080 Pixel.","stockQty":34,"category":["Electronics"]},
+        {"id":18,"title":"Mixer","img":"src/img/mixer-1.webp","price":105,"description":"Convenience without compromising results. With it's lightweight design, the Beautiful Mixer can mix a wide variety of ingredients with 12 optimal speed settings to fit your every need.","stockQty":67,"category":["Electronics"]},
+        {"id":21,"title":"LCD TV","img":"src/img/tv-01.webp","price":560,"description":"This TV packs incredible details into a 4K Ultra High Definition picture. The R6 series also features High Dynamic Range (HDR) that boosts color and contrast, and Motion Rate 120 technology which makes it easier to follow fast action scenes. Plus, with Roku TV operating system (OS), it's easier to watch what you love with access to thousands of streaming channels, apps, and live TV. Giving everyone in your household something to enjoy.","stockQty":20,"category":["Electronics"]},
+        {"id":22,"title":"Eyeshadow - Brown Script","img":"src/img/eyeshadow.webp","price":40,"description":"Brown Script is a moderately warm-toned, medium brown with a matte finish.","stockQty":100,"category":["Health and Beauty"]},
+        {"id":23,"title":"The Alchemist - Paulo Coelho","img":"src/img/book.png","price":28,"description":"The Alchemist (Portuguese: O Alquimista) is a novel by Brazilian author Paulo Coelho which was first published in 1988. Originally written in Portuguese, it became a widely translated international bestseller","stockQty":200,"category":["Books"]},
+        {"id":24,"title":"Ziaja Goat's Milk night creme","img":"src/ziaja.jpg","price":10,"description":"Supplements the deficit of natural nourishing substances. By strengthening the natural lipid coat protects the skin from dehydration. Has remarkable soothing properties. Effectively firms the skin.","stockQty":115,"category":["Health and Beauty"]},
+        {"id":25,"title":"Derviš i smrt - Meša Selimović ","img":"src/img/dervis-i-smrt.jpg","price":25,"description":"Glavni motiv romana je potraga za smislom života. Glavni junak je derviš Ahmed Nurudin koji je ujedno i narator. U romanu se bori za svog brata koji je u zatvoru i susreće sa izazovima i trudi se da uvek postupi ispravno.","stockQty":250,"category":["Books"]},
+       
+    ],
+    "categories":[
+        {"id":1,"name":"Electronics","color":"blue"},
+        {"id":2,"name":"Books","color":"blue"},
+        {"id":3,"name":"Health and beauty","color":"blue"}
+
+    ],
+    "admins":[
+        {"id":1,"name":"Arnela","surname":"Ombasa","email":"arnela.ombasa@gmail.com","password":"12345678"}
+    ]
+}
